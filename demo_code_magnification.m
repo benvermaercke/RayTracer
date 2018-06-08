af9bb0eb895233e099828088d062b5b814520d86
Ben Vermaercke <ben.vermaercke@gmail.com>
2018-06-08 09:17:37 +0200

    expanded magnification demo - corrected spread and added 3rd bundle - expanded Y axis

--- demo_code_magnification.m
+++ demo_code_magnification.m
@@ -10,9 +10,9 @@ RT.add_lens('radii',[100 -100],'spatial_location',[150 0],'height',25.4*1.7,'thi
 
 RT.add_absorber('height',100,'spatial_location',[250 0])
 
-RT.add_bundle('bundle_type',1,'nRays',11,'center_location',[-50 0],'initial_direction',0,'angle_spread',.6)
-RT.add_bundle('bundle_type',1,'nRays',11,'center_location',[-50 5],'initial_direction',0,'angle_spread',.6,'color',[0 0 1])
-
+RT.add_bundle('bundle_type',1,'nRays',11,'center_location',[-50 0],'initial_direction',0,'angle_spread',.4)
+RT.add_bundle('bundle_type',1,'nRays',11,'center_location',[-50 5],'initial_direction',0,'angle_spread',.25,'color',[0 0 1])
+RT.add_bundle('bundle_type',1,'nRays',11,'center_location',[-50 -2.5],'initial_direction',0,'angle_spread',.25,'color',[0 1 1])
 %%
 RT.draw_space()
 
@@ -22,6 +22,12 @@ RT.run_simulation()
 %%
 RT.update_space()
 
+%% expand Y axis
+cur_range=get(gca,'yLim');
+set(gca,'yLim',cur_range/3)
+axis square
+
+% reveal some aberrations in the focal plane curvature
 
 
 
