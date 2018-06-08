ab2c0ebbf8a2ee9d355fc6d957f9d31ac0449d18
Ben Vermaercke <ben.vermaercke@gmail.com>
2018-06-08 09:11:08 +0200

    updated LSM example - added Y-axis rescaling

--- demo_code_LSM.m
+++ demo_code_LSM.m
@@ -3,12 +3,15 @@ clc
 
 addpath(genpath(fileparts(mfilename('fullpath'))))
 
+%%
+
 RT=RayTracer();
 
-RT.add_lens('radii',[50 -50],'spatial_location',[0 0],'height',25.4*1,'thickness',5)
+RT.add_lens('radii',[50 -50],'spatial_location',[0 0],'height',25.4*1.2,'thickness',5)
 RT.add_lens('radii',[100 -100],'spatial_location',[150 0],'height',25.4*2,'thickness',7)
 
-RT.add_mirror('height',40,'spatial_location',[-50 0],'angle',pi/4*3-.02)
+galvo_angle=pi/4*3+.11; % max range is .11
+RT.add_mirror('height',40,'spatial_location',[-50 0],'angle',galvo_angle)
 
 RT.add_absorber('height',100,'spatial_location',[250 0])
 
@@ -23,7 +26,10 @@ RT.run_simulation()
 %%
 RT.update_space()
 
-
+%% expand Y axis
+cur_range=get(gca,'yLim');
+set(gca,'yLim',cur_range/3)
+axis square
 
 
 
