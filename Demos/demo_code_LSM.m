2fda4461d4e14f46f65c95021097ebd1ffd461f6
Ben Vermaercke <ben.vermaercke@gmail.com>
2018-06-13 00:17:22 +0200

    added ETL and png export

--- Demos/demo_code_LSM.m
+++ Demos/demo_code_LSM.m
@@ -7,14 +7,24 @@ addpath(genpath(fileparts(mfilename('fullpath'))))
 
 RT=RayTracer();
 
-RT.add_lens('radii',[50 -50],'spatial_location',[0 0],'height',25.4*1.4,'thickness',6,'kappa',-4)
-RT.add_lens('radii',[100 -100],'spatial_location',[150 0],'height',25.4*2,'thickness',6,'kappa',-4)
+% ETL? 
+RT.add_lens('radii',[-1000 -100],'spatial_location',[-50 -24],'angle',pi/2,'height',25.4*1.4,'thickness',4,'kappa',-3.5) % offset A
+RT.add_lens('radii',[-100 100],'spatial_location',[-50 -19],'angle',pi/2,'height',25.4*1.4,'thickness',2,'kappa',-3.5) % offset B
 
-RT.add_absorber('height',100,'spatial_location',[250 0])
+RT.add_lens('radii',[40 -1000],'spatial_location',[-50 -30],'angle',pi/2,'height',25.4*1.4,'thickness',6,'kappa',-3.5) % ETL
 
-RT.add_bundle('bundle_type',2,'nRays',5,'center_location',[-50 -50],'initial_direction',pi/2,'beam_spread',15)
+RT.add_lens('radii',[50 -50],'spatial_location',[0 0],'height',25.4*1.4,'thickness',6,'kappa',-3.5) % scan lens
+RT.add_lens('radii',[100 -100],'spatial_location',[150 0],'height',25.4*2,'thickness',6,'kappa',-3.5) % tube lens
 
-switch 5% max range is .11
+RT.add_absorber('height',25,'spatial_location',[250 20])
+RT.add_lens('radii',[1 -1]*60,'spatial_location',[250 0],'height',25.4,'thickness',6,'kappa',-3.5)
+RT.add_absorber('height',25,'spatial_location',[250 -30])
+
+RT.add_absorber('height',100,'spatial_location',[320 0])
+
+RT.add_bundle('bundle_type',2,'nRays',9,'center_location',[-50 -50],'initial_direction',pi/2,'beam_spread',15)
+
+switch 3% max range is .11
     case 1
         galvo_angle=pi/4*3-.06;
     case 2
@@ -31,6 +41,7 @@ RT.add_mirror('height',40,'spatial_location',[-50 0],'angle',galvo_angle)
 
 
 %%
+RT.x_range=[-75 350];
 RT.draw_space()
 
 %%
@@ -49,11 +60,15 @@ if 0
     %% print current layout
     fname=sprintf('export/LSM/LSM_sim_a_%.d.eps',round(galvo_angle/pi*180))
     core.savec(fname)
-    print(gcf,'-depsc',fname)
-    
-    
+    print(gcf,'-depsc',fname) 
 end
 
+if 0
+    %% print current layout
+    fname=sprintf('export/LSM/LSM_sim_a_%.d.png',round(galvo_angle/pi*180))
+    core.savec(fname)
+    print(gcf,'-dpng',fname) 
+end
 
 
 
