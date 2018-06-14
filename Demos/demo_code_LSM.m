514c216768533675268008f694e27fcad034d836
Ben Vermaercke <ben.vermaercke@gmail.com>
2018-06-14 10:12:06 +0200

    added comments

--- Demos/demo_code_LSM.m
+++ Demos/demo_code_LSM.m
@@ -7,24 +7,28 @@ addpath(genpath(fileparts(mfilename('fullpath'))))
 
 RT=RayTracer();
 
-% ETL? 
+% ETL 
 RT.add_lens('radii',[-1000 -100],'spatial_location',[-50 -24],'angle',pi/2,'height',25.4*1.4,'thickness',4,'kappa',-3.5) % offset A
-RT.add_lens('radii',[-100 100],'spatial_location',[-50 -19],'angle',pi/2,'height',25.4*1.4,'thickness',2,'kappa',-3.5) % offset B
+RT.add_lens('radii',[-100 100],'spatial_location',[-50 -19.5],'angle',pi/2,'height',25.4*1.4,'thickness',2,'kappa',-3.5) % offset B
 
-RT.add_lens('radii',[40 -1000],'spatial_location',[-50 -30],'angle',pi/2,'height',25.4*1.4,'thickness',6,'kappa',-3.5) % ETL
+RT.add_lens('radii',[400 -1000],'spatial_location',[-50 -27-10],'angle',pi/2,'height',25.4*1.4,'thickness',6,'kappa',-3.5) % ETL
 
+% telescope
 RT.add_lens('radii',[50 -50],'spatial_location',[0 0],'height',25.4*1.4,'thickness',6,'kappa',-3.5) % scan lens
 RT.add_lens('radii',[100 -100],'spatial_location',[150 0],'height',25.4*2,'thickness',6,'kappa',-3.5) % tube lens
 
+% objective
 RT.add_absorber('height',25,'spatial_location',[250 20])
 RT.add_lens('radii',[1 -1]*60,'spatial_location',[250 0],'height',25.4,'thickness',6,'kappa',-3.5)
 RT.add_absorber('height',25,'spatial_location',[250 -30])
 
+% sample
 RT.add_absorber('height',100,'spatial_location',[320 0])
 
+% laser beam
 RT.add_bundle('bundle_type',2,'nRays',9,'center_location',[-50 -50],'initial_direction',pi/2,'beam_spread',15)
 
-switch 3% max range is .11
+switch 1% max range is .11
     case 1
         galvo_angle=pi/4*3-.06;
     case 2
